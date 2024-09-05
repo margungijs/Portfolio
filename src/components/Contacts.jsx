749d99b22d488019f6402b0bfee831c138800f54
margungijs <131168640+margungijs@users.noreply.github.com>
2024-09-05 10:27:48 +0300

    Removed funny image

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -19,16 +19,13 @@ const Contacts = () => {
         <div className="flex flex-col md:p-10 p-4 items-center h-fit min-h-screen max-w-screen" id = "contact">
             <h1 className="md:text-7xl text-5xl font-bold text-red-300 text-center">Contact me</h1>
             <div className="flex md:flex-row flex-col md:items-center w-full h-full md:p-10 p-2 gap-4">
-                <div className="md:w-1/2 w-full flex flex-col gap-4 justify-center hidden-animate-left">
+                <div className="w-full flex flex-col gap-4 justify-center hidden-animate-left">
                     <h1 className="text-center">If you have a message to send me, feel free to contact me at either of these forms of communication!</h1>
                     <h1 className="md:indent-4">Phone number:</h1>
                     <h1 className="text-center md:text-5xl text-3xl font-bold text-blue-300">+371 26684646</h1>
                     <h1 className="md:indent-4">E-mail:</h1>
                     <h1 className="text-center md:text-5xl text-3xl font-bold text-red-300 break-all">marcis.jansons@gmail.com</h1>
                     <h1 className = "md:mt-10 mt-4 text-center">This is the end of my portfolio! Have a good day!</h1>
-                </div>
-                <div className="md:w-1/2 w-full h-full hidden-animate-right">
-                    <img src={Marcis} alt="Marcis" className="w-full h-full rounded-lg object-cover shadow-lg"/>
                 </div>
             </div>
         </div>
